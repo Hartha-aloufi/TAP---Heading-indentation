f101bf44980efebc7f2af9eb3eede65fb7adb344
Hartha Aloufi <user.email>
2024-06-29 15:59:07 +0300

    solution

--- main.js
+++ main.js
@@ -18,22 +18,56 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 
+const calcIndentation = (parentArray) => {
+  const indentation = [];
 
+  for (let i = 0; i < parentArray.length; i++) {
+    const parentOfI = parentArray[i];
+    const parentIndentation = indentation[parentOfI];
 
+    if (parentOfI === undefined) {
+      indentation[i] = 0;
+    } else {
+      // calc currennt indentation
+      const currentIndentation = parentIndentation + 1;
+      indentation[i] = currentIndentation;
+    }
+  }
 
-const renderHeadingTree = (data) => {
+  return indentation;
+}
 
+const buildParentTree = (headingList) => {
+  const parent = []
+
+  for (let i = headingList.length - 1; i >= 0; i--) {
+    const currLevel = headingList[i].data.level; // 3
+
+    for (let j = i - 1; j >= 0; j--) {
+      if (headingList[j].data.level < currLevel) {
+        parent[i] = j;
+        break;
+      }
+    }
+  }
+
+  return parent;
+}
+
+const renderHeadingTree = (headingList, indent) => {
   const headingTree = document.getElementById('heading-tree');
   headingTree.innerHTML = '';
   const ul = document.createElement('ul');
-  headingTree.appendChild(ul);
-  data.blocks.forEach(block => {
-    if (block.type === 'header') {
-      const li = document.createElement('li');
-      li.textContent = block.data.text;
-      ul.appendChild(li);
-    }
+
+  headingList.forEach((block, idx) => {
+    const li = document.createElement('li');
+    const indentation = indent[idx];
+    li.innerHTML = Array(indentation).fill('&nbsp;&nbsp;&nbsp;&nbsp;').join('') + block.data.text
+    ul.appendChild(li);
   });
+
+
+  headingTree.appendChild(ul);
 }
 
 const editor = new EditorJS({
@@ -50,8 +84,13 @@ const editor = new EditorJS({
   },
   onChange: (api, event) => {
     const data = editor.save().then((outputData) => {
-      console.log('Article data: ', outputData)
-      renderHeadingTree(outputData)
+      const headingList = outputData.blocks.filter(block => block.type === 'header')
+      const parent = buildParentTree(headingList)
+      console.log('Parent array: ', parent);
+      console.log('Indentation array: ', calcIndentation(parent));
+
+
+      renderHeadingTree(headingList, calcIndentation(parent))
     }
     );
   }
